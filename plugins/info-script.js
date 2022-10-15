6cf716e717a9a671ca2a89f3ddd488047e416deb
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-10-15 10:46:17 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -8,8 +8,8 @@ let str = `*https://wa.me/6285380166282*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://youtu.be/lqa4vJIXTi8",
-    title: 'https://youtu.be/lqa4vJIXTi8',
+    mediaUrl: "https://youtu.com",
+    title: 'https://youtu.com',
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
