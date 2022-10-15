40fdf86b519e359eb15d59a7967c41e010394780
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-10-15 16:57:20 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -4,12 +4,12 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://wa.me/6285380166282*`
+let str = `*https://wa.me/6283805685278*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://youtu.com",
-    title: 'https://youtu.com',
+    mediaUrl: "https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz",
+    title: 'Mau SC? Cht sini',
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
