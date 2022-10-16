e9d37cc373c3b77ae8fc3b075bc23bd43074f799
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-10-16 17:05:12 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -700,10 +700,10 @@ export async function participantsUpdate({ id, participants, action }) {
                                 membercount: groupMetadata.participants.length
                             })
     conn.sendButtonDoc(id, text, wm, action == 'add' ? 'ᴡᴇʟᴄᴏᴍᴇ' : 'sᴀʏᴏɴᴀʀᴀᴀ', action === 'add' ? '.intro' : 'ok', fkontak, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://instagram.com/Xiao_yan_21",
+    mediaUrl: "https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz",
     mediaType: 2, 
-    description: "https://youtu.be/-TleC8rbDT8", 
-    title: 'Elaina-MultiDevice',
+    description: "https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz", 
+    title: '©Saxia˘Botz',
     body: wm,
     thumbnail: await(await fetch(action === 'add' ? wel : lea)).buffer(),
     sourceUrl: sig
